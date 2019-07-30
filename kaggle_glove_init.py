2375466c6750bf795ecbe84c246a9b9d8c4efc8a
Cui <cui_yaxin@outlook.com>
2019-07-30 21:51:45 +0800

    去除output文件夹

--- kaggle_glove_init.py
+++ kaggle_glove_init.py
@@ -1,17 +1,15 @@
 import os
 from shutil import copyfile
 
+# kaggle kernel需要连接网络
+
 print("../input:", os.listdir("../input"))
 # 列出Glove向量文件名
 
-if not os.path.exists("../output"):
-    os.mkdir("../output")
+if not os.path.exists("./vector_cache"):
+    os.mkdir("./vector_cache")
 
-if not os.path.exists("../output/vector_cache"):
-    os.mkdir("../output/vector_cache")
-
-if not os.path.exists("../output/vector_cache/glove.6B.300d.txt"):
-    copyfile("../input/glove.6B.300d.txt", "../output/vector_cache/glove.6B.300d.txt")
+if not os.path.exists("./vector_cache/glove.6B.300d.txt"):
+    copyfile("../input/glove.6B.300d.txt", "./vector_cache/glove.6B.300d.txt")
     # 把想要使用的Glove词向量转移到output/vector_cache文件夹中，将vector_cache文件夹作为缓存文件夹
-print("../output:", os.listdir("../output"))
-print("../output/vector_cache:", os.listdir("../output/vector_cache"))
+print("./vector_cache:", os.listdir("./vector_cache"))
